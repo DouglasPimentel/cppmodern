5c1c61cf09291928e8933d97ad322840459e721b
Douglas Pimentel <dev.pimentel@gmail.com>
2022-10-22 02:31:29 -0300

    Test fibonacci with positive inputs

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,12 +2,16 @@
 #include <helloworld.h>
 #include <fibonacci.h>
 
-TEST(HelloWorldTest, BasicAssertions) {
+TEST(HelloWorldTest, BasicAssertions)
+{
   std::string msg = greeting("John Doe");
   EXPECT_EQ(msg, "Welcome, John Doe");
 }
 
-TEST(FibonacciTest, BasicAssertion) {
+TEST(FibonacciTest, HandlesPositiveInput)
+{
+  EXPECT_EQ(1, fibonacci(1));
+  EXPECT_EQ(1, fibonacci(2));
   EXPECT_EQ(55, fibonacci(10));
   EXPECT_EQ(144, fibonacci_2(12));
 }
